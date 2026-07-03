efd4d6b166ec9aa2a2feb9e916e06125866e9323
Marian Koncek <marian.koncek@mailbox.org>
2026-07-03 13:06:59 +0200

    Add fopen* family of functions

--- src/preload.c
+++ src/preload.c
@@ -7,6 +7,9 @@
 #include <stdlib.h>
 #include <errno.h>
 
+#include <stdio.h>
+#include <dirent.h>
+
 #include <dlfcn.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -21,6 +24,13 @@ DECLARE_FUNCTION_POINTER(open);
 DECLARE_FUNCTION_POINTER(open64);
 DECLARE_FUNCTION_POINTER(openat);
 DECLARE_FUNCTION_POINTER(openat64);
+
+DECLARE_FUNCTION_POINTER(fopen);
+DECLARE_FUNCTION_POINTER(fopen64);
+DECLARE_FUNCTION_POINTER(freopen);
+DECLARE_FUNCTION_POINTER(freopen64);
+DECLARE_FUNCTION_POINTER(opendir);
+DECLARE_FUNCTION_POINTER(fdopendir);
 
 DECLARE_FUNCTION_POINTER(execve);
 DECLARE_FUNCTION_POINTER(fexecve);
@@ -41,6 +51,13 @@ static void constructor(void)
 	ASSIGN_FUNCTION_POINTER(open64);
 	ASSIGN_FUNCTION_POINTER(openat);
 	ASSIGN_FUNCTION_POINTER(openat64);
+	
+	ASSIGN_FUNCTION_POINTER(fopen);
+	ASSIGN_FUNCTION_POINTER(fopen64);
+	ASSIGN_FUNCTION_POINTER(freopen);
+	ASSIGN_FUNCTION_POINTER(freopen64);
+	ASSIGN_FUNCTION_POINTER(opendir);
+	ASSIGN_FUNCTION_POINTER(fdopendir);
 	
 	ASSIGN_FUNCTION_POINTER(execve);
 	ASSIGN_FUNCTION_POINTER(fexecve);
@@ -117,6 +134,54 @@ int openat64(int fd, const char* file, int oflag, ...)
 	record_openat_path(fd, file);
 	errno = errno_orig;
 	return openat64_orig(fd, file, oflag, mode);
+}
+
+FILE* fopen(const char* path, const char* mode)
+{
+	int errno_orig = errno;
+	record_path(path);
+	errno = errno_orig;
+	return fopen_orig(path, mode);
+}
+
+FILE* fopen64(const char* path, const char* mode)
+{
+	int errno_orig = errno;
+	record_path(path);
+	errno = errno_orig;
+	return fopen64_orig(path, mode);
+}
+
+FILE* freopen(const char* path, const char* mode, FILE* stream)
+{
+	int errno_orig = errno;
+	record_path(path);
+	errno = errno_orig;
+	return freopen_orig(path, mode, stream);
+}
+
+FILE* freopen64(const char* path, const char* mode, FILE* stream)
+{
+	int errno_orig = errno;
+	record_path(path);
+	errno = errno_orig;
+	return freopen64_orig(path, mode, stream);
+}
+
+DIR* opendir(const char* name)
+{
+	int errno_orig = errno;
+	record_path(name);
+	errno = errno_orig;
+	return opendir_orig(name);
+}
+
+DIR* fdopendir(int fd)
+{
+	int errno_orig = errno;
+	record_fd(fd);
+	errno = errno_orig;
+	return fdopendir_orig(fd);
 }
 
 int execve(const char* path, char* const argv[], char* const envp[])
